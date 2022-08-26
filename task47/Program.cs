93aac860f6b01f72bd1ddac867cd1a88d089e061
Kristina <ya.kep0602@yandex.ru>
2022-08-27 01:06:10 +0300

    Решение задачи №47

---
+++ task47/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.Write("Введите количество строк: "); 
+
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите количество столбцов: ");  
+
+int columns = Convert.ToInt32(Console.ReadLine());
+
+int[,] matrix = new int[rows, columns]; 
+
+for (int i = 0; i < matrix.GetLength(0); i++) 
+{
+
+for (int j = 0; j < matrix.GetLength(1); j++) ;
+}
+
+double[,] array = new double[rows, columns];
+
+Random random = new Random();
+
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < columns; j++)
+    {
+        array[i, j] = random.NextDouble() * 100; 
+
+        Console.Write( array[i, j]);
+    }
+    Console.WriteLine();
+}
